3681b4917f7d9a13811e7d6b356e454d5245d862
Luke Eastman <LukeAEastman@gmail.com>
2020-09-18 12:40:15 -0700

    set up board htm using javascript

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,45 @@
 
+var body = document.body;
 
+var board = document.createElement('table');
+body.prepend(board);
+board.style.emptyCells =  'show';
+
+boardBody = document.createElement('tbody')
+var rowOne = document.createElement('tr')
+var rowTwo = document.createElement('tr')
+var rowThree = document.createElement('tr')
+boardBody.append(rowOne);
+boardBody.append(rowTwo);
+boardBody.append(rowThree);
+board.append(boardBody);
+
+var rowOneColumnOne = document.createElement('td');
+var rowOneColumnTwo = document.createElement('td');
+var rowOneColumnThree = document.createElement('td');
+rowOne.append(rowOneColumnOne);
+rowOne.append(rowOneColumnTwo);
+rowOne.append(rowOneColumnThree);
+
+var rowTwoColumnOne = document.createElement('td');
+var rowTwoColumnTwo = document.createElement('td');
+var rowTwoColumnThree = document.createElement('td');
+rowTwo.append(rowTwoColumnOne);
+rowTwo.append(rowTwoColumnTwo);
+rowTwo.append(rowTwoColumnThree);
+
+var rowThreeColumnOne = document.createElement('td');
+var rowThreeColumnTwo = document.createElement('td');
+var rowThreeColumnThree = document.createElement('td');
+rowThree.append(rowThreeColumnOne);
+rowThree.append(rowThreeColumnTwo);
+rowThree.append(rowThreeColumnThree);
+
+
+var squares = board.querySelectorAll('td')
+for (var i = 0; i < squares.length; i++) {
+  squares[i].style.border = '5px solid black';
+  squares[i].style.height = '50px'
+  squares[i].style.width = '50px'
+  squares[i].backgroundColor = 'black';
+}
