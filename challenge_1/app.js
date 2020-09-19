acecceeec7db6f15690aa5ab65f13f78e43215a5
Luke Eastman <LukeAEastman@gmail.com>
2020-09-18 19:23:10 -0700

    winner goes first after winning

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,166 +1,4 @@
-//--------------------view------------------------------------------
-var body = document.body;
-
-var board = document.createElement('table');
-body.prepend(board);
-board.style.emptyCells =  'show';
-
-boardBody = document.createElement('tbody');
-var rowOne = document.createElement('tr');
-var rowTwo = document.createElement('tr');
-var rowThree = document.createElement('tr');
-rowOne.classList.add('row-one');
-rowTwo.classList.add('row-two');
-rowThree.classList.add('row-three');
-
-boardBody.append(rowOne);
-boardBody.append(rowTwo);
-boardBody.append(rowThree);
-board.append(boardBody);
-
-var rowOneColumnOne = document.createElement('td');
-var rowOneColumnTwo = document.createElement('td');
-var rowOneColumnThree = document.createElement('td');
-rowOne.append(rowOneColumnOne);
-rowOne.append(rowOneColumnTwo);
-rowOne.append(rowOneColumnThree);
-
-var rowTwoColumnOne = document.createElement('td');
-var rowTwoColumnTwo = document.createElement('td');
-var rowTwoColumnThree = document.createElement('td');
-rowTwo.append(rowTwoColumnOne);
-rowTwo.append(rowTwoColumnTwo);
-rowTwo.append(rowTwoColumnThree);
-
-var rowThreeColumnOne = document.createElement('td');
-var rowThreeColumnTwo = document.createElement('td');
-var rowThreeColumnThree = document.createElement('td');
-rowThree.append(rowThreeColumnOne);
-rowThree.append(rowThreeColumnTwo);
-rowThree.append(rowThreeColumnThree);
-
-var resetGameButton = document.createElement('button');
-resetGameButton.innerHTML = 'Reset Game';
-board.append(resetGameButton);
-resetGameButton.addEventListener('click', (event) => resetGame());
-
-
-var wins = document.createElement('div');
-wins.classList.add('wins');
-var XWins = document.createElement('div');
-XWins.classList.add('x-wins');
-XWins.innerHTML = '"X" Wins: 0';
-wins.append(XWins);
-var OWins = document.createElement('div');
-OWins.classList.add('o-wins');
-OWins.innerHTML = '"O" Wins: 0';
-wins.append(XWins);
-wins.append(OWins);
-body.append(wins);
-var squares = board.querySelectorAll('td')
-for (var i = 0; i < squares.length; i++) {
-  squares[i].style.border = '5px solid black';
-  squares[i].style.height = '50px'
-  squares[i].style.width = '50px'
-  squares[i].backgroundColor = 'black';
-  squares[i].addEventListener('click', (event) => handleClick(event));
-}
-//--------------------controller----------------------------------------
-var handleClick = (event) => {
-  if (isOver()) {
-    alert('reset game to play again');
-    return;
-  }
-  var square = event.target
-  markSquare(square);
-  var gameIsOver = gameOver();
-  if (gameIsOver) {
-    updateWins();
-    alert(gameIsOver);
-  }
-}
-
-var updateWins = () => {
-  XWins.innerHTML = `"X" Wins :${getWins('x')}`;
-  OWins.innerHTML = `"O" Wins :${getWins('o')}`;
-}
-
-
-var getIndex = (square) => {
-  if (square.parentElement.classList.contains('row-one')) {
-    return 0;
-  } else if (square.parentElement.classList.contains('row-two')) {
-    return 1;
-  } else if (square.parentElement.classList.contains('row-three')) {
-    return 2;
-  }
-}
-
-var markSquare = (square) => {
-  var x = square.cellIndex;
-  var y = getIndex(square);
-  if (isPlayerX()) {
-    if (getPiece(y, x) === '-') {
-      setPiece(y, x);
-      square.innerHTML = 'X';
-      switchTurns();
-    } else {
-      alert('seats taken')
-    }
-  } else if (!isPlayerX()) {
-    if (getPiece(y, x) === '-') {
-      setPiece(y, x);
-      square.innerHTML = 'O';
-      switchTurns();
-    } else {
-      alert('seats taken')
-    }
-  }
-}
-
-var resetGame = () => {
-  var squares = board.querySelectorAll('td')
-  for (var i = 0; i < squares.length; i++) {
-    squares[i].innerHTML = '';
-  }
-  resetBoard();
-  resetTurnCount();
-  resetOver();
-  resetPlayer();
-}
-
-var switchTurns = () => {
-  state.player = !state.player;
-  incrementTurnCount();
-}
-
-var gameOver = () => {
-  var diagonals = checkDiagonals();
-  var horizontals = checkRows();
-  var verticals = checkColumns();
-
-  if (diagonals) {
-    setOver();
-    return diagonals;
-  }
-  if (horizontals) {
-    setOver();
-    return horizontals;
-  }
-  if (verticals) {
-    setOver();
-    return verticals;
-  }
-  if(getTurnCount() === 9) {
-    setOver();
-    return 'a draw!';
-  }
-  return false;
-}
-
-
 //--------------------model-----------------------------
-
 
 var state = {
   board: [['-', '-', '-'],['-', '-', '-'],['-', '-', '-']],
@@ -168,9 +6,15 @@ var state = {
   xWins: 0,
   oWins: 0,
   player: true,
-  over: false
+  over: false,
 }
-
+var setWinner = (player) => {
+  if (player === 'x') {
+    state.player = true;
+  } else {
+    state.player = false;
+  }
+}
 var resetOver = () => {
   state.over = false;
 }
@@ -210,16 +54,15 @@ var getTurnCount = () => {
   return state.turnCount;
 }
 
+var switchTurns = () => {
+  state.player = !state.player;
+  incrementTurnCount();
+}
+
 var incrementTurnCount = () => {
   state.turnCount++;
 }
-var incrementWins = (player) => {
-  if (player === 'x') {
-    state.xWins++
-  } else {
-    state.oWins++
-  }
-}
+
 var getWins = (player) => {
   if (player === 'x') {
     return state.xWins;
@@ -236,6 +79,7 @@ var incrementWins = (player) => {
   }
 }
 var getWins = (player) => {
+  setWinner(player);
   if (player === 'x') {
     return state.xWins;
   } else {
@@ -316,7 +160,6 @@ var majorDiagonalOf = (player) => {
   }
 }
 
-
 var minorDiagonalOf = (player) => {
   if (player === 'x') {
     return isX(0, 2) && isX(1, 1) && isX(2, 0);
@@ -324,3 +167,158 @@ var minorDiagonalOf = (player) => {
     return isO(0, 2) && isO(1, 1) && isO(2, 0);
   }
 }
+
+//--------------------view------------------------------------------
+var body = document.body;
+
+var board = document.createElement('table');
+body.prepend(board);
+board.style.emptyCells =  'show';
+
+boardBody = document.createElement('tbody');
+var rowOne = document.createElement('tr');
+var rowTwo = document.createElement('tr');
+var rowThree = document.createElement('tr');
+rowOne.classList.add('row-one');
+rowTwo.classList.add('row-two');
+rowThree.classList.add('row-three');
+
+boardBody.append(rowOne);
+boardBody.append(rowTwo);
+boardBody.append(rowThree);
+board.append(boardBody);
+
+var rowOneColumnOne = document.createElement('td');
+var rowOneColumnTwo = document.createElement('td');
+var rowOneColumnThree = document.createElement('td');
+rowOne.append(rowOneColumnOne);
+rowOne.append(rowOneColumnTwo);
+rowOne.append(rowOneColumnThree);
+
+var rowTwoColumnOne = document.createElement('td');
+var rowTwoColumnTwo = document.createElement('td');
+var rowTwoColumnThree = document.createElement('td');
+rowTwo.append(rowTwoColumnOne);
+rowTwo.append(rowTwoColumnTwo);
+rowTwo.append(rowTwoColumnThree);
+
+var rowThreeColumnOne = document.createElement('td');
+var rowThreeColumnTwo = document.createElement('td');
+var rowThreeColumnThree = document.createElement('td');
+rowThree.append(rowThreeColumnOne);
+rowThree.append(rowThreeColumnTwo);
+rowThree.append(rowThreeColumnThree);
+
+var resetGameButton = document.createElement('button');
+resetGameButton.innerHTML = 'Reset Game';
+board.append(resetGameButton);
+resetGameButton.addEventListener('click', (event) => resetGame());
+
+
+var wins = document.createElement('div');
+wins.classList.add('wins');
+var XWins = document.createElement('div');
+XWins.classList.add('x-wins');
+XWins.innerHTML = '"X" Wins: 0';
+wins.append(XWins);
+var OWins = document.createElement('div');
+OWins.classList.add('o-wins');
+OWins.innerHTML = '"O" Wins: 0';
+wins.append(XWins);
+wins.append(OWins);
+body.append(wins);
+var squares = board.querySelectorAll('td')
+for (var i = 0; i < squares.length; i++) {
+  squares[i].style.border = '5px solid black';
+  squares[i].style.height = '50px'
+  squares[i].style.width = '50px'
+  squares[i].backgroundColor = 'black';
+  squares[i].addEventListener('click', (event) => handleClick(event));
+
+  var updateWins = () => {
+    XWins.innerHTML = `"X" Wins :${getWins('x')}`;
+    OWins.innerHTML = `"O" Wins :${getWins('o')}`;
+  }
+}
+//--------------------controller----------------------------------------
+var handleClick = (event) => {
+  if (isOver()) {
+    alert('reset game to play again');
+    return;
+  }
+  var square = event.target
+  markSquare(square);
+  var gameIsOver = gameOver();
+  if (gameIsOver) {
+    updateWins();
+    alert(gameIsOver);
+  }
+}
+
+var getIndex = (square) => {
+  if (square.parentElement.classList.contains('row-one')) {
+    return 0;
+  } else if (square.parentElement.classList.contains('row-two')) {
+    return 1;
+  } else if (square.parentElement.classList.contains('row-three')) {
+    return 2;
+  }
+}
+
+var markSquare = (square) => {
+  var x = square.cellIndex;
+  var y = getIndex(square);
+  if (isPlayerX()) {
+    if (getPiece(y, x) === '-') {
+      setPiece(y, x);
+      square.innerHTML = 'X';
+      switchTurns();
+    } else {
+      alert('seats taken')
+    }
+  } else if (!isPlayerX()) {
+    if (getPiece(y, x) === '-') {
+      setPiece(y, x);
+      square.innerHTML = 'O';
+      switchTurns();
+    } else {
+      alert('seats taken')
+    }
+  }
+}
+
+var resetGame = () => {
+  var squares = board.querySelectorAll('td')
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].innerHTML = '';
+  }
+  resetBoard();
+  resetTurnCount();
+  resetOver();
+}
+
+var gameOver = () => {
+  var diagonals = checkDiagonals();
+  var horizontals = checkRows();
+  var verticals = checkColumns();
+
+  if (diagonals) {
+    setOver();
+    return diagonals;
+  }
+  if (horizontals) {
+    setOver();
+    return horizontals;
+  }
+  if (verticals) {
+    setOver();
+    return verticals;
+  }
+  if(getTurnCount() === 9) {
+    setOver();
+    return 'a draw!';
+  }
+  return false;
+}
+
+
